845886f18d966559b8c225146603d82c3487c9a4
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-04-03 15:22:43 +0200

    added

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 web = webdriver.ChromeOptions()
 web.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=web)
-driver.get("https://za.indeed.com/l-johannesburg,-gauteng-jobs.html?vjk=89455e1b791bd189")
+driver.get("https://ozh.github.io/cookieclicker/")
 
-button = driver.find_element(By.XPATH, '//*[@id="text-input-what"]')
-button.send_keys("Software Engineering")
-click_ = driver.find_element(By.XPATH, '//*[@id="jobsearch"]/div/div[2]/button')
-click_.click()
+# Pause the program for 2 seconds
+time.sleep(10)
+
+# button = driver.find_element(By.XPATH, '//*[@id="langSelect-EN"]')
+# button.send_keys(Keys.ENTER)
+click_ = driver.find_element(By.ID, "bigCookie")
+
+
+
+
+start_time = time.time()
+while True:
+    # current_time = time.time()
+    # elapsed = current_time - start_time
+    for i in range(10):
+        click_.click()
+    # print("Running...")
+    element = driver.find_elements(By.CSS_SELECTOR, ".product.unlocked.enabled")
+    # if elapsed >= 50:
+    #     print("5 seconds reached. Stopping program.")
+    #     break
+# click_.click()
